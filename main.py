f16873aace9b2b05f924fd1943d78c1cbf81870d
AKASHKATYAN <akashkatyan2020@gmail.com>
2026-01-24 13:57:40 +0530

    separating folders

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from fastapi import FastAPI,HTTPException
-from typing import Annotated,Optional
-from pydantic import BaseModel,Field
 import uvicorn as uv
 from utils import load_data,write_data
 import pandas as pd
+from schema.data_validation import Cards,ViewCardsDetails,Recharge,Refund
 
 #Initializing FastAPI
 app=FastAPI()
-
-#Validating Fields
-class Cards(BaseModel):
-    card_id:str=Field(...,min_length=10,max_length=10,description='Card_id of the card a 10 digit unique number')
-    balance:float=Field(...,gt=0,le=10000,description='The amount credited to the card after recharge')
-    name:str=Field(...,min_length=1,description='Name of the user')
-    contact_no:str=Field(...,min_length=10,max_length=10,description='Contact number of the user')
-
-class ViewCardsDetails(BaseModel):
-    card_id:str=Field(...,min_length=10,max_length=10,description='Card_id of the card a 10 digit unique number')
-
-class Recharge(BaseModel):
-    card_id:str=Field(...,min_length=10,max_length=10,description='Card_id of the card a 10 digit unique number')
-    amount:float=Field(...,gt=100,description='Amount credited to be credited to the card.') 
-class Refund(BaseModel):
-    card_id:str=Field(...,min_length=10,max_length=10,description='Card_id of the card a 10 digit unique number')
 
 #Home page
 @app.get("/")
